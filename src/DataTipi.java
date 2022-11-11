2b505b6bbd81465cbea0fb093bf07dc298f0d4e4
muratAVC <110787002+muratAVC@users.noreply.github.com>
2022-11-11 22:48:58 +0300

    zombiler

--- src/DataTipi.java
+++ src/DataTipi.java
@@ -8,16 +8,12 @@ public class DataTipi {
             long x=girdi.nextLong();
             System.out.println("Girdiiniz "+x+" Degeri");
             System.out.println((x>=-128 && x<=127) ? ("*  byte"): (""));
-            // if(x>=-128 && x<=127)System.out.println("* byte");
 
             System.out.println((x>=-32768 && x<=32768) ? ("*  short  "): (""));
-            //if(x>=-32768 && x<=32768) System.out.println("* short");
 
             System.out.println((x>=-2147483648 && x<=2147483647) ? ("*  integer  "): (""));
-            //if(x>=-2147483648 && x<=2147483647) System.out.println("* integer");
 
-            System.out.println((x<-2147483648 | x>2147483647) ? ("*  long  "): (""));
-            //if(x<-2147483648 | x>2147483647) System.out.print("* Long");
+            System.out.println((x<-2147483648 || x>2147483647) ? ("*  long  "): (""));
 
             //Complete the code
         }
